3726f1e7a5a6d3baba4a3acfaf98b382ca53350f
Taehoon Kim <tkim@taehoons-mbp.attlocal.net>
2021-06-11 23:59:18 -0400

    Ex 2.5-2.7

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,6 @@
 # report.py
 #
-# Exercise 2.4
+# Exercise 2.5
 import csv
 
 def read_portfolio(filename):
@@ -11,7 +11,36 @@ def read_portfolio(filename):
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
+            holding = {headers[0]: row[0], 
+                        headers[1]: int(row[1]), 
+                        headers[2]: float(row[2])}
             portfolio.append(holding)    
-    return portfolio
     
+    return portfolio
+
+# Exercise 2.6
+def read_prices(filename):
+
+    prices = {}
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                print('list index out of range', row)
+    
+    return prices
+
+# Exercise 2.7
+total_gain = 0
+
+portfolio = read_portfolio('Data/portfolio.csv')
+prices = read_prices('Data/prices.csv')
+
+for stock in portfolio:
+    curr_price = prices[stock['name']]
+    total_gain += stock['shares'] * (curr_price - stock['price'])
+
+print(f'Total gain: {total_gain:0.2f}')
