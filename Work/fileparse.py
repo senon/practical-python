4117357ee34e5c0be5a1d5a61f79bb7d6b7aa4bf
taehoon <taehoon@gmail.com>
2021-06-19 18:04:13 -0400

    Ex 8.2

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -2,6 +2,8 @@
 #
 # Exercise 3.4-3.7
 import csv
+import logging
+log = logging.getLogger(__name__)
 
 def parse_csv(file, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
     '''
@@ -37,8 +39,8 @@ def parse_csv(file, select=None, types=None, has_headers=True, delimiter=',', si
                 row = [ func(val) for func, val in zip(types, row) ]
             except ValueError as e:
                 if not silence_errors:
-                    print(f"Row {rowno}: Couldn't convert {row}")
-                    print(f"Row {rowno}: Reason {e}")
+                    log.warning("Row %d: Couldn't convert %s", rowno, row)
+                    log.debug("Row %d: Reason %s", rowno, e)
                 continue
 
         # Make a dictionary or a tuple
