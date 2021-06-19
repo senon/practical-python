f718dee51aaa6f3da21a549a0d62fb957de7ee26
taehoon <taehoon@gmail.com>
2021-06-19 11:48:27 -0400

    Ex 7.9

--- Work/stock.py
+++ Work/stock.py
@@ -1,26 +1,23 @@
 # stock.py
 #
 # Exercise 4.1
+from typedproperty import String, Integer, Float
 class Stock:
-    __slots__ = ('name', '_shares', 'price')
+    '''
+    An instance of a stock holding consisting of name, shares, and price.
+    '''
+    name = String('name')
+    shares = Integer('shares')
+    price = Float('price')
+
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
     
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f'Stock({self.name!r}, {self.shares!r}, {self.price!r})'
     
-    @property
-    def shares(self):
-        return self._shares
-    
-    @shares.setter
-    def shares(self, value):
-        if not isinstance(value, int):
-            raise TypeError('Expected int')
-        self._shares = value
-
     @property
     def cost(self):
         return self.shares * self.price
